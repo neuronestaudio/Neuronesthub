13dc239b2bc1aec6a39c710dd8c7f38d51aa54e5
neuronestaudio <neuronestaudio@gmail.com>
2026-04-17 00:08:07 +0000

    Restore static homepage, fix build errors, update vercel config

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import SoundHub from './pages/SoundHub';
 import RadioApp from './pages/RadioApp';
 import LandingPage from './pages/LandingPage';
