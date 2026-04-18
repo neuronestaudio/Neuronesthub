cf980ea7ad0f852f7bb344f8872317701a50b8d6
neuronestaudio <neuronestaudio@gmail.com>
2026-04-18 05:42:25 +0000

    fix: soundhub-app now serves the real SoundHub component (yellow/black UI)

--- soundhub-app/src/App.tsx
+++ soundhub-app/src/App.tsx
@@ -1,12 +1,12 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import SoundHub from './pages/SoundHub';
+import SoundHub from '../../src/pages/SoundHub';
 
 function App() {
   return (
     <BrowserRouter basename="/soundhub-app">
       <Routes>
         <Route path="/" element={<SoundHub />} />
-        {/* Add more routes as needed */}
+        <Route path="/:slug" element={<SoundHub />} />
       </Routes>
     </BrowserRouter>
   );
